30bf3f67d25c8c8a590ec04db5c99b327390a7ff
AndreyBugrov <bugrovandriy@yandex.ru>
2024-05-10 16:52:50 +0300

    plus one refactoring

--- experiments.py
+++ experiments.py
@@ -4,9 +4,28 @@ import argparse
 import shlex
 import sys
 import os
+import logging
 
-def error_message(msg: str):
-    print(f"Error: {msg}")
+
+def create_function_dict() -> dict[str, set[str]]:
+    all_function_set = {'base', 'base_omp','tr', 'tr_omp', 'tr_omp_simd', 'row', 'row_omp', 'row_opt', 'row_opt_omp',
+                        'row_simd', 'row_omp_simd', 'row_opt_omp_simd', 'row_opt_simd', 'strassen', 'strassen_omp',
+                        'strassen_rec_omp'}
+    omp_function_set = {item for item in all_function_set if 'omp' in item}
+    single_thread_function_set = all_function_set.difference(omp_function_set)
+    simd_function_set = {item for item in all_function_set if 'simd' in item}
+    omp_function_set_no_simd = omp_function_set.difference(simd_function_set)
+    single_thread_function_set_no_simd = single_thread_function_set.difference(simd_function_set)
+    function_names_dict = {'all': all_function_set, 'omp': omp_function_set,
+                        'single_thread': single_thread_function_set, 'omp_no_simd': omp_function_set_no_simd,
+                        'single_thread_no_simd': single_thread_function_set_no_simd}
+    for item in all_function_set:
+        function_names_dict[item] = {item}
+    return function_names_dict
+
+
+def critical_message(msg: str, logger):
+    logger.critical(f"{msg}")
     sys.exit(-1)
 
 
@@ -15,7 +34,7 @@ def compile_source(source_file_list: list[str], bin_path: str, optimization_flag
     cmd = shlex.split(args)
     compiler_errors = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[1]
     if compiler_errors:
-        error_message('\n'+compiler_errors.decode('utf-8'))
+        critical_message('Compilation errors:\n'+compiler_errors.decode('utf-8'))
 
 
 def create_cache_list_file(file_path: str):
@@ -50,11 +69,11 @@ def set_min_core_frequency_limit(frequency, core_num):
     subprocess.Popen(line, shell=True).communicate()
 
 
-def run_matrix_exp(bin_path: str, function_name: str, matrix_sizes: list[int], exp_num: int, device_type: str, frequency: int, is_temporary: bool):
+def run_matrix_exp(bin_path: str, function_name: str, matrix_sizes: list[int], exp_num: int, device_type: str, frequency: int, is_temporary: bool, logger):
     min_n = matrix_sizes[0]
     max_n = matrix_sizes[1] + 1
     step = matrix_sizes[2]
-    csv_file_name = function_name + '_' + device_type + '_' + str(frequency / (1000*1000)) + 'GHz'
+    csv_file_name = function_name + '_' + device_type + '_' + str(frequency / (1000*1000)) + 'GHz' +'.csv'
     if is_temporary:
         csv_file_name = os.path.join('csv_results_tmp', csv_file_name)
     else:
@@ -70,30 +89,22 @@ def run_matrix_exp(bin_path: str, function_name: str, matrix_sizes: list[int], e
             full_out = proc.communicate()
             out = full_out[0].decode('utf-8')
             if full_out[1]:
-                print(f"Unhandled C++ exception:\n{full_out[1].decode('utf-8')}") #proc.communicate()[0].decode('utf-8')
+                logger.error(f"Unhandled C++ exception:\n{full_out[1].decode('utf-8')}")
             result = ';'.join(out[:-1].split('\n'))
             row = result.split(';')
             row.insert(0, num)
             writer.writerow(row)
+    logger.info(f'Results saved to {csv_file_name}')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Matrix multiplication experiments run automatization", formatter_class=argparse.RawTextHelpFormatter)
-    simd_function_set = {'tr_omp_simd', 'row_simd', 'row_opt_simd','row_omp_simd', 'row_opt_omp_simd'}
-
-    omp_function_set = {'base_omp', 'tr_omp', 'tr_omp_simd', 'row_omp', 'row_omp_simd', 'row_opt_omp', 'row_opt_omp_simd', 'strassen_omp', 'strassen_rec_omp'}
-    omp_no_simd_function_set = omp_function_set.difference(simd_function_set)
-    single_thread_function_set = {'base', 'row', 'row_simd', 'row_opt', 'row_opt_simd', 'tr', 'strassen'}
-    single_thread_no_simd_function_set = single_thread_function_set.difference(simd_function_set)
-    all_function_list = omp_function_set.union(single_thread_function_set)
-    all_no_simd_function_set = all_function_list.difference(simd_function_set)
-    all_function_list = list(all_function_list)
-    all_function_list.sort()
-    function_choices = all_function_list
-    function_choices.extend(['omp', 'omp_no_simd' 'all', 'all_no_simd', 'single_thread', 'single_thread_no_simd'])
-    parser.add_argument('-f', '--function-names', choices=function_choices, nargs='+', metavar='FUNC',
+    function_names_dict = create_function_dict()
+    function_names_list = list(function_names_dict.keys())
+    function_names_list.sort()
+    parser.add_argument('-f', '--function-names', choices=function_names_dict.keys(), nargs='+', metavar='FUNC',
                         help="Matrix multiplication functions short names"
-                        f"\nChoices:\n{function_choices}\n'all': all functions, 'omp': omp functions only, "
+                        f"\nChoices:\n{function_names_list}\n'all': all functions, 'omp': omp functions only, "
                         "single_thread': single-thread functions only. You can add '_no_simd' to 'all', 'omp' or "
                         "'single_thread' to avoid simd version of functions",
                         required=True)
@@ -103,65 +114,67 @@ if __name__ == '__main__':
     parser.add_argument('-n', '--exp-num', help="Number of experiments with equal parameters", type=int, required=True)
     parser.add_argument('-d', '--device-name', help="RISC-V device name", choices=["sf2", "lichee", "mango", "kendryte", "x86"], required=True)
     parser.add_argument('--is-temporary', help="Should the results be saved to temporary directory or not", required=True, choices=['true', 'false'])
+    parser.add_argument('-r', '--recompile',help='Should source files be recompiled', choices=['true', 'false'], default='true')
     args = parser.parse_args()
-    function_names = args.function_name
+    function_names = args.function_names
     matrix_sizes = args.matrix_sizes
     opt_level = args.opt_level
     exp_num = args.exp_num
     device_name = args.device_name
-    is_temporary = True if args.is_temporary == 'true' else False 
+    recompile = args.recompile
+
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(level=logging.INFO)
+    is_temporary = True if args.is_temporary == 'true' else False
+    if is_temporary:
+        logger.warning('Results will be saved to temporary directory')
+    else:
+        logger.warning('Results will be saved to the base directory')
     if int(exp_num) < 1:
-        error_message("choose at least one experiment!")
+        critical_message("Choose at least one experiment!", logger)
     if matrix_sizes[2] <= 0:
-        error_message("step should be more than 0!")
+        critical_message("Step should be more than 0!", logger)
     if matrix_sizes[0] > matrix_sizes[1]:
-        error_message("min_n should be less or equal max_n!")
+        critical_message("\"min_n\" should be less or equal \"max_n\"!", logger)
 
     type_handlings = {'release': '-O2', 'opt': '-O3', 'fast': '-Ofast'}
     
     root_source_dir = 'mat_opt'
     root_bin_dir = 'bin'
 
-    function_name_list = {}
-    if 'all' in function_names:
-        function_name_list = all_function_list
-    else:
-        function_name_list = {item for item in function_names if item != 'omp' and item != 'omp_no_simd' and item != 'single_thread' and item != 'single_thread_no_simd'}
-        if 'omp' in function_names:
-            function_name_list = function_name_list.union(omp_function_set)
-        if 'single_thread' in function_names:
-            function_name_list = function_name_list.union(single_thread_function_set)
-        if 'all_no_simd' in function_names:
-            unction_name_list = function_name_list.union(all_no_simd_function_set)
-        else:
-            if 'omp_no_simd' in function_names:
-                function_name_list = function_name_list.union(omp_no_simd_function_set)
-            if 'single_thread_no_simd' in function_names:
-                function_name_list = function_name_list.union(single_thread_no_simd_function_set)
-
-    function_name_list = list(function_name_list)
-    function_name_list.sort()
+    function_names_set = set()
+    for function_item in function_names:
+        function_names_set = function_names_set.union(function_names_dict[function_item])
+    logger.debug(f'Chosen functions: {function_names_set}')
 
     bin_path = os.path.join(root_bin_dir, device_name + '_exp')
     source_file_list = [os.path.join(root_source_dir, 'common.cpp'),
                             os.path.join(root_source_dir, 'multiplication.cpp'),
                             os.path.join(root_source_dir, 'experiment.cpp'),
                             os.path.join(root_source_dir, 'main_experiment.cpp')]
-    print("Preprocessing...")
-    compile_source(source_file_list, bin_path, type_handlings[opt_level]) # recompilation does not garantees cold start (garanteed only for first function)
+    logger.info("Start of preprocessing phase")
+    if recompile:
+        logger.info('Compilation')
+        compile_source(source_file_list, bin_path, type_handlings[opt_level]) # recompilation does not garantees cold start (garanteed only for first function)
+    else:
+        logger.warning('Compilation is skipped')
 
     create_cache_list_file('cache.txt')
 
     core_nums = get_available_cores()
     frequencies = get_min_max_frequencies()
-    for core in core_nums:
-        set_min_core_frequency_limit(frequencies[1], core)
+    try:
+        logger.info('Frequency setting')
+        for core in core_nums:
+            set_min_core_frequency_limit(frequencies[1], core)
 
-    print("Conducting an experiment...")    
-    for function_item in function_name_list:
-        print(f'Process \"{function_item}\" function...')
-        run_matrix_exp(bin_path, function_item, matrix_sizes, exp_num, device_name, frequencies[1], is_temporary)
-    
-    for core in core_nums:
-        set_min_core_frequency_limit(frequencies[0], core)
-    print("Done!")
+        logger.info("Start of the experiment execution phase")
+        for function_item in function_names_set:
+            logger.info(f'Process \"{function_item}\" function')
+            run_matrix_exp(bin_path, function_item, matrix_sizes, exp_num, device_name, frequencies[1], is_temporary, logger)
+    except KeyboardInterrupt:
+        critical_message('Program has been interrupted', logger)
+    finally:
+        for core in core_nums:
+            set_min_core_frequency_limit(frequencies[0], core)
+        print("Done!")
